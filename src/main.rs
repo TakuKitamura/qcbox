19b3913d5f3aeb1e332a3c0d322c0f4bc83c46cd
TakuKitamura <takukitamura.io@gmail.com>
2020-09-28 18:20:43 +0900

    refacta

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 mod qc;
 use num_complex::Complex;
 fn main() {
-  println!("Hello qcbox!");
+  let qc: qc::Qbit = qc::Qbit {
+    zero: Some(Complex::new(0.0, 0.0)),
+    one: Some(Complex::new(1.0, 0.0)),
+  };
+  println!("had(had(qc))");
+  println!("{:?}", qc::get_amplitude(&qc.zero));
+  println!("{:?}", qc::get_amplitude(&qc.one));
+
+  let qc = qc::qc_had(qc);
+  println!("{:?}", qc::get_amplitude(&qc.zero));
+  println!("{:?}", qc::get_amplitude(&qc.one));
+
+  let qc = qc::qc_had(qc);
+  println!("{:?}", qc::get_amplitude(&qc.zero));
+  println!("{:?}", qc::get_amplitude(&qc.one));
 }
